e34f109bfea3f087b6127730282ddd0dff020ebb
Mahmod-Mashlah <mashlahmahmod@gmail.com>
2024-04-17 11:56:08 +0300

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_3_getx/main.dart';
+import 'package:project_lambda_dental/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

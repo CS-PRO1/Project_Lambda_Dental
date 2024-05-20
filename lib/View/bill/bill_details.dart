1a34ff4c6d8a67016f8c13fbd4b1ec5cfd201339
Mahmod-Mashlah <mashlahmahmod@gmail.com>
2024-05-20 16:24:41 +0300

    bill DETAILS NANANANANANANA

--- lib/View/bill/bill_details.dart
+++ lib/View/bill/bill_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:project_lambda_dental/shared/component/constants.dart';
 
 class BillDetails extends GetView {
   const BillDetails({super.key});
@@ -18,61 +19,68 @@ class BillDetails extends GetView {
         centerTitle: true,
         title: Text('Bill'.tr + '001'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          children: [
-            ListView.separated(
-                physics: BouncingScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) =>
-                    catItemBuilder(context, index),
-                itemCount: 8,
-                separatorBuilder: (BuildContext context, int index) =>
-                    Container(
-                      height: 1,
-                      color: Colors.grey,
-                    )),
-            Container(
-              color: Colors.grey[200],
-              width: double.infinity,
-              height: 60,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Final Bill'.tr),
-                  Text('16.000.000 '),
-                ],
-              ),
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              ListView.separated(
+                  physics: BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) =>
+                      catItemBuilder(context, index),
+                  itemCount: 14,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Container(
+                        height: 1,
+                        color: Colors.grey,
+                      )),
+              Container(
+                color: Colors.grey[200],
+                width: double.infinity,
+                height: 60,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Final Bill'.tr),
+                    Text('16.000.000 '),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
   catItemBuilder(context, index) {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: InkWell(
-        onTap: () {},
+    return InkWell(
+      onTap: () {
+        Get.toNamed('/orderdetails');
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Flexible(
               child: Text(
-                '2.000.000',
-                style: Theme.of(context).textTheme.headline6,
+                '#001',
+                style: TextStyle(fontSize: 18, color: cyan600),
                 overflow: TextOverflow.ellipsis,
               ),
             ),
             Column(
               children: [
-                Text('Material:'.tr + ' ' + 'Zircon'.tr),
-                Text('Pieces:'.tr + ' ' + '5'),
+                Text('Patient Name:'.tr + ' ' + 'Sali'.tr),
+                Text('Date:'.tr + ' ' + '5/4/2024'),
               ],
             ),
-            // Icon(Icons.arrow_forward_ios_rounded),
+            Text(
+              '6,000,000',
+              style: TextStyle(fontSize: 22, color: cyan400),
+            ),
           ],
         ),
       ),
